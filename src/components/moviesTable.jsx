3097ce9d1abb46ffa8d2a9f787a5d49a33950864
anjumriashat <riashat93@yahoo.com>
2019-04-12 18:18:21 +0600

    Added table header,body,pagination

---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Table from "./common/table";
+
+class MoviesTable extends Component {
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "delete",
+      content: movie => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Remove
+        </button>
+      )
+    }
+  ];
+
+  render() {
+    const { movies, onSort, sortColumn } = this.props;
+
+    return (
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+    );
+  }
+}
+
+export default MoviesTable;
